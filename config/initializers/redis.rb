15f79fa16e7a7516f799b6cd1f8795b9b2d21688
Kozlov Evgeny <eeeeevgen@gmail.com>
2017-07-06 02:28:06 +0300

    redis settings for heroku

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,1 +1,4 @@
-$redis = Redis::Namespace.new("promo", :redis => Redis.new)
+# $redis = Redis::Namespace.new("promo", :redis => Redis.new)
+
+uri = URI.parse(ENV["REDIST_URL"] || "redis://localhost:6379/" )
+$redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
