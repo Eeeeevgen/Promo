de84eec49ef5df5a7cf6f1a68596258a27d56e03
Kozlov Evgeny <eeeeevgen@gmail.com>
2017-07-06 02:32:13 +0300

    redis settings for heroku

--- app/interactions/lb/lb_helper.rb
+++ app/interactions/lb/lb_helper.rb
@@ -3,7 +3,8 @@ module LbHelper
 
   private
     def lb
-      lb = CompetitionRankingLeaderboard.new('likes',  {:page_size => 6})
+      # lb = CompetitionRankingLeaderboard.new('likes',  {:page_size => 6})
+      lb = CompetitionRankingLeaderboard.new('likes',  options, redis_connection: Redis.current)
     end
 
     def options
