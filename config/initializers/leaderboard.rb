d8cdd0d106b4669cff73e1f44402cc24f43b97bb
Kozlov Evgeny <eeeeevgen@gmail.com>
2017-07-25 22:17:52 +0300

    redis correction

--- config/initializers/leaderboard.rb
+++ config/initializers/leaderboard.rb
@@ -1,6 +1,6 @@
 require 'competition_ranking_leaderboard'
 
-uri = URI.parse(ENV['REDIST_URL'] || 'redis://localhost:6379/')
+uri = URI.parse(ENV['REDIS_URL'] || 'redis://localhost:6379/')
 redis = Redis.new(host: uri.host, port: uri.port, password: uri.password)
 
 LB = CompetitionRankingLeaderboard.new('likes',
