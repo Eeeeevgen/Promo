b27a5177907d108fbeae8074f51d9088d8b54164
Kozlov Evgeny <eeeeevgen@gmail.com>
2017-09-07 09:35:29 +0300

    disabled aws

--- app/uploaders/base_uploader.rb
+++ app/uploaders/base_uploader.rb
@@ -1,12 +1,12 @@
 class BaseUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
-  # Choose what kind of storage to use for this uploader:
-  if Rails.env.production?
-    storage :fog
-  else
-    storage :file
-  end
+  # if Rails.env.production?
+  #   storage :fog
+  # else
+  #   storage :file
+  # end
+  storage :file
 
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
